54b493e164e889a513ac815e9d73a85ba9f9854e
dane-dalton <danedalton@sbcglobal.net>
2022-04-19 14:01:31 -0500

    partials, pages, different routes

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,22 @@ const Author = require('../models/author')
 const router = express.Router()
 
 // All authors route
-router.get('/', (req,res) => {
-    res.render('authors/index')
+router.get('/', async (req,res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+    
+    
 })
 
 //New author route
@@ -13,8 +27,20 @@ router.get('/new', (req, res) => {
 })
 
 //Create new authors
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true })
 const db = mongoose.connection
@@ -18,6 +19,7 @@ app.set('views', __dirname + '/views')
 app.set ('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.use(express.json())
 
